cd66c76bda5776470a3d2a82cf44b30f8d8d180d
haon <haoony@naver.com>
2024-11-15 17:58:01 +0900

    d

--- javascript/6.class/7_extends.js
+++ javascript/6.class/7_extends.js
@@ -1,29 +1,9 @@
 //class간의 상속관계
 /* e 동물원에서 동물관리프로그램을 생성하려고한다
     흰색사자,회색사자,흰색호랑이,회색호랑이,푸들,말티즈 가 있따.
-
     객체모델링=> Lion, Tiger ,Dog Class
-
-    <Lion class >
-    속성 : name, color , emoji , taste(육식인지 채식인지)
-    메소드 : display(이모지출력), sleep ,eat
-
-    <Tiger class >
-    속성 : name, color , emoji , taste(육식인지 채식인지)
-    메소드 : display(이모지출력), sleep ,eat
-
-    <Dog class >
-    속성 : name, color , emoji , taste(육식인지 채식인지)
-    메소드 : display(이모지출력), sleep ,eat
-
-공통적인 속성이나 매소드를 뽑아서  부모클래스를 만듬 거기서 상속받아서ㅏ 구현됨
-    lion,tiger,dog 클래스를 모델링하여 부모클래스를 생성한다 ==>Animal
-
-Animal 클래스 속성 : name, color , emoji , taste
-            매소드 : display(이모지출력), sleep ,eat
-
-    -클래스 다이어그램 : 
 */
+
 class Animal {      //name.color.emoji.taste
     constructor(name,color,emoji,taste){
         this.name=name;
@@ -39,9 +19,8 @@ class Animal {      //name.color.emoji.taste
 
 class Lion extends Animal {    //lion class는 animal class의 자식이다 => class 클래스명 extends 부모클래스명
     constructor(name,color,emoji,taste){
-        super(name,color,emoji,taste);  //!!!super는 맨 첫줄에 와야한다!!! 자식field를 지우면 super 로 내용을 채워줘야한다
-    } //super 는class밖에서 사용할수없윰
-//메소드내용도 중복이니까 자식한테는 안적어도됌
+        super(name,color,emoji,taste);  
+    } 
 }
 
 class Tiger extends Animal{   //Tiger에 age라는 field 가 추가됨
