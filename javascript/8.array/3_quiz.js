de25c331b078ed983c0ff5a4b575370fde7f3ee1
haon <haoony@naver.com>
2024-11-18 21:26:24 +0900

    k

--- javascript/8.array/3_quiz.js
+++ javascript/8.array/3_quiz.js
@@ -8,16 +8,16 @@ for(let i = 0;i <fruits.length;i++){
 }
 console.log(`-------------------`);
 
-//1-2. 함수를 만들어서 변경하기 (함수는 다른데서도 쓰니까 내용을 fruit에 맞게 적는게아니라 모든거에 적용되도록 적어야함)
+//1-2. 함수를 만들어서 변경하기
 // 파라미터(배열객체,old,new)  old= 사과 new= 딸기  
 //교체한 값을 콘솔에 출력
 function arrayReplace (array,oldValue,newValue) {
-    let result = '';  //가로로 출력하기위해서 중간저장값 하나 만든거임 구구단할떄도 이거 있었엉
+    let result = ''; 
     for(let i = 0;i <array.length;i++){
         if(array[i]=== oldValue) array[i] = newValue ;
        result += `${array[i]}\t`
     }
-    console.log(result);   //result 대신 array 찍어도 나오긴함 배열에 값들어가서 나옴
+    console.log(result); 
 }
 arrayReplace(fruits,'🍎','🍓');
 
@@ -35,7 +35,8 @@ console.log(`-------------------`);
 //     return array;
 // }    //얘를 아래꺼로 수정함(얕은복사하기위해)
 
-function arrayReplace2 (array,oldValue,newValue) {   //함수로 객체를 받아서 사용할떄는 카피본을 하나 만들고 사용(=>얕은복사)
+//함수로 객체를 받아서 사용할떄는 카피본을 하나 만들고 사용(=>얕은복사)
+function arrayReplace2 (array,oldValue,newValue) {   
     let resultArray = Array.from(array); 
     for(let i = 0;i <resultArray.length;i++){
         if(resultArray[i]=== oldValue) resultArray[i] = newValue ;   
