c06fe4d7e89c27aa87b41475c1a361b020c11f36
haon <haoony@naver.com>
2024-11-21 16:06:35 +0900

    ㄴ

--- javascript/12.asynchronous/1_basic.js
+++ javascript/12.asynchronous/1_basic.js
@@ -7,9 +7,7 @@ function a () {
     
 }
 function b () {
-    // setTimeout(함수,딜레이시간ms);   //setTimeout은 parseInt와 같은 전역함수임 
-    // setTimeout(function(){} ,1000); //   에로우형식으로 쓰거냐 그냥 일반함수형식으로 써도상관없움
-    setTimeout(() => {console.log(`1초후 setTimeout 함수 실행`)},1000); //   1000=1초
+    setTimeout(() => {console.log(`1초후 setTimeout 함수 실행`)},1000);
     console.log(`b function`);
     
 }
@@ -20,11 +18,12 @@ function c () {
 }
 
 a();
+
+
 /*결과값
 b function
 c function
 a function
 1초후 setTimeout 함수 실행
 
-setTimeout은 비동기식으로처리하는함수 라 0초로 줘도 맨 마지막에 출력이된다.
 */
