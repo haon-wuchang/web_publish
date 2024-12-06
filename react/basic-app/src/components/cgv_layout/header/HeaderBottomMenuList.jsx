04013083613333bc196a58657cb814f1f46adc2e
haon <haoony@naver.com>
2024-12-06 16:30:04 +0900

    s

--- react/basic-app/src/components/cgv_layout/header/HeaderBottomMenuList.jsx
+++ react/basic-app/src/components/cgv_layout/header/HeaderBottomMenuList.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import HeaderBottomMenu from "./HeaderBottomMenu.jsx";
+import { fetchJson } from "../js/commons.js";
 
 export default function HeaderBottomMenuList() {
     const [names, SetNames] =useState([]);
-    useEffect(() =>{
-        fetch('/data/cgv_header.json')
-            .then(data => data.json())
-            .then(jsonData => SetNames(jsonData.bottomMenuList))
-            .catch(error => console.log(error))
+    useEffect(() =>{       
+                fetchJson('/data/cgv_header.json')   //함수 사용
+                    .then(result => SetNames(result.bottomMenuList))  
+                    .catch(error => console.log(error));
+                    // fetch('/data/cgv_header.json')
+        //     .then(data => data.json())
+        //     .then(jsonData => SetNames(jsonData.bottomMenuList))
+        //     .catch(error => console.log(error))
     },[]);
+
 
     return(
         <ul>
--- react/basic-app/src/components/cgv_layout/header/HeaderTopMenuList.jsx
+++ react/basic-app/src/components/cgv_layout/header/HeaderTopMenuList.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import HeaderTopMenu from "./HeaderTopMenu.jsx"
+import { fetchJson } from "../js/commons.js";
 
 export default function HeaderTopMenuList() {
     const [list,setList] = useState([]);
     useEffect(() => {
-        fetch('/data/cgv_header.json')
-            .then((data) => data.json())
-            .then((jsonData) => setList(jsonData.topMenuList))
-            .catch(error => console.log(error))
+        // fetch('/data/cgv_header.json')
+        //     .then((data) => data.json())
+        //     .then((jsonData) => setList(jsonData.topMenuList))
+        //     .catch(error => console.log(error))
+        fetchJson('/data/cgv_header.json')
+            .then(data => setList(data.topMenuList))
+            .catch(error => console.log(error));
     },[]);
 
     return(
