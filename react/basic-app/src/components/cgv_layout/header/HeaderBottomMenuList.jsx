c868208b0645a015eed6f292e8d46242025c4bb5
haon <haoony@naver.com>
2024-12-06 15:23:28 +0900

    s

--- react/basic-app/src/components/cgv_layout/header/HeaderBottomMenuList.jsx
+++ react/basic-app/src/components/cgv_layout/header/HeaderBottomMenuList.jsx
@@ -1,18 +1,18 @@
+import { useEffect, useState } from "react";
 import HeaderBottomMenu from "./HeaderBottomMenu.jsx";
 
 export default function HeaderBottomMenuList() {
-    const names =[
-        {'name' : '영화'},
-        {'name' : '극장'},
-        {'name' : '예매'},
-        {'name' : '스토어'},
-        {'name' : '이벤트'},
-        {'name' : '혜택'},
-    ];
+    const [names, SetNames] =useState([]);
+    useEffect(() =>{
+        fetch('/data/cgv_header.json')
+            .then(data => data.json())
+            .then(jsonData => SetNames(jsonData.bottomMenuList))
+            .catch(error => console.log(error))
+    },[]);
 
     return(
         <ul>
-            {names.map(menu => 
+            {names && names.map(menu => 
                 <li>
                     <HeaderBottomMenu name={menu.name} />                                           
                 </li>                
--- react/basic-app/src/components/cgv_layout/header/HeaderTopMenuList.jsx
+++ react/basic-app/src/components/cgv_layout/header/HeaderTopMenuList.jsx
@@ -1,33 +1,19 @@
+import { useEffect, useState } from "react";
 import HeaderTopMenu from "./HeaderTopMenu.jsx"
 
 export default function HeaderTopMenuList() {
-    const list =[
-        {
-            'href' : '#' , 
-            'src' : 'https://img.cgv.co.kr/R2014/images/common/ico/loginPassword.png' , 
-            'name' : '로그인'
-        },
-        {
-            'href' : '#' , 
-            'src' : 'https://img.cgv.co.kr/R2014/images/common/ico/loginJoin.png' , 
-            'name' : '회원가입'
-        },
-        {
-            'href' : '#' , 
-            'src' : 'https://img.cgv.co.kr/R2014/images/common/ico/loginMember.png' , 
-            'name' : 'My CGV'
-        },
-        {
-            'href' : '#' , 
-            'src' : 'https://img.cgv.co.kr/R2014/images/common/ico/loginCustomer.png' , 
-            'name' : '고객센터'
-        }
-    ];
+    const [list,setList] = useState([]);
+    useEffect(() => {
+        fetch('/data/cgv_header.json')
+            .then((data) => data.json())
+            .then((jsonData) => setList(jsonData.topMenuList))
+            .catch(error => console.log(error))
+    },[]);
 
     return(
         <nav>
             <ul>
-                {list.map((item) => 
+                {list && list.map((item) => 
                 <li>
                     <HeaderTopMenu 
                         href = {item.href} 
