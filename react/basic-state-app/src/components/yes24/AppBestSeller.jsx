23c210f13facc22da7d9c8eae06695a526645e48
haon <haoony@naver.com>
2024-12-10 17:00:57 +0900

    ㅇ

--- react/basic-state-app/src/components/yes24/AppBestSeller.jsx
+++ react/basic-state-app/src/components/yes24/AppBestSeller.jsx
@@ -1,18 +1,16 @@
 import './bestBook.css';
 import BestBook from './BestBook.jsx';
 
-import React from 'react';
-
 export default function AppBestSeller() {
     return (
         <div>
-            
+            <BestBook />
         </div>
     );
 }
 
 
-//vscode 단축키
-// 스니팻? 
+
+//vscode 단축키 추가하는 방법
 //상단메뉴 -파일-프리퍼런스-컨피그 스니팻-뉴글로벌어쩌구-파일이름.json 이라고 지정하기(.json)꼭적어야함- 파일하나뜸-이그젬플부분처럼 만들어주면도미
 // 새로뜬파일은  json 파일이다 여기파일형식에 맞춰서 내용 잘적음녀 댄다
