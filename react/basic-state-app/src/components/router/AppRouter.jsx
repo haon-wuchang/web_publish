5a00fad41cedd87b84245b30bbf1b54e19581d00
haon <haoony@naver.com>
2024-12-20 14:19:09 +0900

    d

--- react/basic-state-app/src/components/router/AppRouter.jsx
+++ react/basic-state-app/src/components/router/AppRouter.jsx
@@ -23,24 +23,5 @@ export default function AppRouter() {
             </Routes>
         </BrowserRouter>
     ); 
-} // path 는 Route 의 기본틀이다  => / 로 요청이오면 엘리먼트안에잇는 데이터를 실행한다
-//라우터에서 컴포넌트 넣을떄는 <Home /> 이케 넣어야대 이름만 달랑 넣으면 안됨
-//about 이라는 값을 받으면 About 컴포넌트를 실행한다  => http://localhost:3000/about 이렇게 주면 About 컴포넌트의 내용이 실행된다
-//path 이름은 다 다르게 줘야한다
+} 
 
-//그래서 path 로 login 페이지 만들고 join 페이지 만들고 다 할수 있다
-//보통 path 에 적는 이름은 소문자 이다
-
-// 이렇게 주소값을 쳐서 넘어가는건 불편하니까 버튼만들어서 about 버튼누르면 About 사이트로 이동할수잇게 설정한다
-
-// Layout 은  레이아웃 틀을 만들어서 진행하려고 한다 혹은 Root 로도 쓰인다
-// 이화면의 디자인은 Layout 이 잡고있다는 뜻이다.
-
-// Layout 안에 홈 어바웃 애들이 들어가야하니까 Layout 을 포함한 Route 가 감싼형태로 작성한것이다
-
-// path 이름이랑 컴포넌트 이름 동일하지 않아도됨 근데 헷갈릴껄 ㅋ 
-
-//<Route index element={<Home />} /> 인덱스는 기본주소값에 보여주는 것이다  http://localhost:3000  Home 의 url 임
-
-
-//    / = 루트 
--- react/basic-state-app/src/components/router/Layout.jsx
+++ react/basic-state-app/src/components/router/Layout.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import  {Outlet, Link} from 'react-router-dom'; // Link는 <a> 태그의 링크역할과 비슷함
+import  {Outlet, Link} from 'react-router-dom';
 import Header from './Header.jsx';
 
 export default function Layout() {
-    return (  // 여기에 레이아웃 디자인을 하면된다 헤더 Outlet 푸터 이케만들면 됨( 바디대신에 Outlet 이다 Outlet은 계속 내용이 바뀌니까  about 인지 login 인지 Outliet이 자동으로 대치가된다)
+    return (  
         <div>
             <Header>
                 <Link to = '/' style={{'padding':'10px'}}>Home</Link>
@@ -19,6 +19,3 @@ export default function Layout() {
         </div>
     );
 }
-
-// <Link to = '이동하려는path이름'>컴포넌트명</Link> 
-// => 해당 컴포넌트로 연결된다
