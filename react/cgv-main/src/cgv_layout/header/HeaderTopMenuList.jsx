3b555084f79cb3175b6e11115ae1ef0bece81d88
haon <haoony@naver.com>
2024-12-24 14:10:45 +0900

    d

--- react/cgv-main/src/cgv_layout/header/HeaderTopMenuList.jsx
+++ react/cgv-main/src/cgv_layout/header/HeaderTopMenuList.jsx
@@ -5,10 +5,6 @@ import { fetchJson } from "../js/commons.js";
 export default function HeaderTopMenuList() {
     const [list,setList] = useState([]);
     useEffect(() => {
-        // fetch('/data/cgv_header.json')
-        //     .then((data) => data.json())
-        //     .then((jsonData) => setList(jsonData.topMenuList))
-        //     .catch(error => console.log(error))
         fetchJson('/data/cgv_header.json')
             .then(data => setList(data.topMenuList))
             .catch(error => console.log(error));
