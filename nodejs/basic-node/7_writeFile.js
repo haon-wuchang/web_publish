8b454561077746aa24368bc46d8dce6c8d4ff042
haon <haoony@naver.com>
2025-01-13 17:41:22 +0900

    ㅇ

--- nodejs/basic-node/7_writeFile.js
+++ nodejs/basic-node/7_writeFile.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 // console.log(fs);
 // fs.writeFile('파일경로',데이터, 콜백함수);
-fs.writeFileSync('./writeme3.txt','민초 살짝 먹고싶을지도',(err)=> console.log('에러욤'));   
+fs.writeFileSync('./writeme3.txt','민초 살짝 먹고싶을지도',(err)=> 
+        console.log('에러욤'));   
 // 읽어올 데이터가 없어서 인자에 에러만 들어간다
 // writeFile 은 비동기로 빼고 밑에 readFile 먼저 실행해서 출력결과가 민초먼저나오고 나중에 에러가 나온다
 
 const data = fs.readFileSync('./writeme3.txt');
 console.log(data.toString());
 
-// 파일관련 작업은 비동기식 처리 이므로, 
-// 하나의 파일을 읽고, 쓰는 작업을 진행한다면 readFile, writeFile () 함수를 사용한다
-// 여러개의 파일을 !! 순서대로 !! 읽고 쓰는 작업을 한다면 readFileSync(), writeFileSync() 함수를 사용한다
+
 
 
 
--- nodejs/express-server/app.js
+++ nodejs/express-server/app.js
@@ -6,25 +6,24 @@ const server = express();
  브라우저가 접속하는 path(경로) : http://localhost:8080/ 
  이면 path : /(root) */
  server.get('/', (req,res)=>{
-    res.send('<h1>express 서버에서 전송합니다</h1>'); // 브라우저는 html 로 데이터를 받으니까 안에 내용은 태그를 사용해서 내용을 작성한다
- });   
- /* (req,res) 는 콜백함수의 디폴트 인자인다 
-  req : 클라이언트가 보내는 요청정보(클라이언트 -> 서버)
-  res : 서버가 클라이언트에게 보내는 응답하는 정보 (서버 -> 클라이언트=브라우저) */
-//브라우저는 서버에서 res 결과를 못받으면 계속 로딩중으로 뜸
+    res.send('<h1>express 서버에서 전송합니다</h1>'); 
+});   
+// 브라우저는 html 로 데이터를 받으니까 안에 내용은 태그를 사용해서 내용을 작성한다
+ /* 
 
  /*브라우저가 접속하는 path(경로) : http://localhost:8080/test  
  이면 path : /test 임 */
 server.get('/test',(req,res)=>{
     res.send('<h1>퓨퓨퓨퓨 </h1>');
-    console.log('/test 전송완료');  // 이거 콘솔은 cmder 에 찍힘용 
+    console.log('/test 전송완료');
 });
 
 //브라우저가 접속하는 path(경로) : http://localhost:8080/param/홍길동 
 //이면 path : /param 임    홍길동 = 데이터임 .path 아님!!!
 server.get('/param/:name',(req,res)=>{
 //server.get('/param/:data',(req,res)=>{   
-    //console.log('req',req);  이케쳐서 cmder 보면 params { name : 홍길동 } 이케 들어가잇음 그래서 로그찍을때 경로를 저렇게 준거임
+    console.log(req);  
+    // 이케쳐서 cmder 보면 params { name : 홍길동 } 이케 들어가잇음 그래서 로그찍을때 경로를 저렇게 준거임
    console.log('name==>',req.params.name);
     res.send(`${req.params.name} 전송완료`);
 });
