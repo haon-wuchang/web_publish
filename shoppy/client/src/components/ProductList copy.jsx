b0137776e28e80ad60f1930fe1db14ff09f6f03c
haon <haoony@naver.com>
2025-01-14 15:28:39 +0900

    2

---
+++ shoppy/client/src/components/ProductList copy.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import ProductAvatar from './ProductAvatar';
+
+export default function ProductList() {
+    const [list,setList] = useState([]); // !!useState 를 통해서 list 변경되면 실시간 업데이트가 된다
+    useEffect(()=>{
+        fetch('/data/products.json')
+            .then((data)=>data.json())
+            .then((jsonData)=>setList(jsonData))  
+            // !! jsondata가 useEffect 안에서 사ㅡ는게 아니라 해당 { } 안에서만 사는거양 그래서 jsondata를 전역변수로 만들라고 useState 쓴거고 
+            .catch(error => console.log(error));
+    },[]);
+
+// 출력리스트 생성
+// 배열만들어서 배ㅕㅇㄹ안에 실제객체(3개씩묶어서 출력하고싶다) 하면 이케하면댕
+// [ [{1},{2},{3}] , [{4},{5},{6}] , [{7}] ]  // 3개씩 끊는거는 list 를  통해서 한다
+
+// 겉에있는 배열먼저 만들기
+    const rows = [];    
+    // 인덱스로 몇개씩할건지 잘라서 쓸거라 맵사용못함
+    // i+=3 인덱스가 3번지까지오면 
+// slice 느 자동으로 배열을 리턴한당
+    for(let i=0; i< list.length; i+=3){
+        rows.push(list.slice(i , i+3));
+    }
+    // console.log(rows);
+    
+
+    return (
+        <div>
+            {rows.map((rowsArray)=>     // rows 는 2차원배열(배열2개)라서 map 도 2번돌린거임 . 겉에 배열 한번 안에 배열 한번 
+                <div className='product-list'>
+                    {rowsArray.map((data) => 
+                        <ProductAvatar img={data.img}/>
+                    )}
+                </div>            
+            )}
+        </div>
+    );
+}
+// 첫번째 맵 돌린거는 겉에 [] 배열안쪽에 배열이 3개가 잇자나 그걸 돌리는거고
+// 두번째 맵은 안쪽에 있는 배열 안에 {} 3개가 잇으니까 이거 배열을 돌린거임ㄴ
+
+
--- shoppy/client/src/components/ProductList.jsx
+++ shoppy/client/src/components/ProductList.jsx
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import ProductAvatar from './ProductAvatar';
+import axios from 'axios';
 
 export default function ProductList() {
-    const [list,setList] = useState([]); // !!useState 를 통해서 list 변경되면 실시간 업데이트가 된다
+    const [list, setList] = useState([]);
     useEffect(()=>{
-        fetch('/data/products.json')
-            .then((data)=>data.json())
-            .then((jsonData)=>setList(jsonData))  
-            // !! jsondata가 useEffect 안에서 사ㅡ는게 아니라 해당 { } 안에서만 사는거양 그래서 jsondata를 전역변수로 만들라고 useState 쓴거고 
-            .catch(error => console.log(error));
-    },[]);
+        axios.get('/data/products.json')
+            .then((res)=>     // => d여기서 가져오는 res 는 {} 오브젝트리터럴로 묶여잇당 여기에 있는 data 를 가져오려면 res.data 이케 하면댄당 d이 data 는 내가 json 에서 만든 데이터ㅣㅁ임용
+                setList(res.data)
+            )
+            .catch(error=> console.log(error));
+    },[])
 
+    const rows = [];    
+    for(let i=0; i < list.length; i += 3) {
+        rows.push(list.slice(i , i+3));
+    }  
 
     return (
         <div>
-            {list&&list.map((item)=>
-                <ProductAvatar img={item.img}/>
+            {rows.map((rowsArray)=>     
+                <div className='product-list'>
+                    {rowsArray.map((data) => 
+                        <ProductAvatar img={data.img}/>
+                    )}
+                </div>            
             )}
         </div>
     );
 }
+// fetch 는 json 으로 바꾸는 과정을 꼭 거쳐야한당 => 자바에서 많이씀
+// axios 는 json 으로 바꿀 필요가 없다 => 리액트.노드에서 많이씀
 
-// 자바스ㅡ크립트 함수인 for 이런거는 return 안에 못들어감용 return 밖에 써야댕 !
-// return 안에는 map, filter ㅁ만 쓸수잇엉
+
+// axios 는  네트워크로 접속을한다 => 따라서 서버와 서버끼리 연동할때 사용한다 (서로다른 시스템을 연동할때 사용)
+//  import axios from 'axios';
+
