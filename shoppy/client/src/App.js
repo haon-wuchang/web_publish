252177e7f424f2aed75961e181f518597a0f6cf8
haon <haoony@naver.com>
2025-01-14 11:18:10 +0900

    3

--- shoppy/client/src/App.js
+++ shoppy/client/src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route,Routes,BrowerRouter} from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowerRouter>
+        <Routes>
+          <Route path='/'element={<Layout />} >          
+            <Route index element={<Home />} /> 
+            <Route path='/all' element={<Products />} /> 
+            <Route path='/cart' element={<Carts />} /> 
+            <Route path='/login' element={<Login />} /> 
+            <Route path='/signup' element={<Signup />} /> 
+          </Route >
+        </Routes>
+      </BrowerRouter>
     </div>
+
   );
 }
 
-export default App;
