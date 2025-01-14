a4410fbc88eebfcdd4cd1209ee7e981304674a47
haon <haoony@naver.com>
2025-01-14 11:46:14 +0900

    5

--- shoppy/client/src/components/Header.jsx
+++ shoppy/client/src/components/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <div>
-            <div>Logo</div> 
-            <nav>
-                <li>Products</li>
-                <li>Cart</li>
-                <li>Login</li>
-                <li>Signup</li>
-            </nav>
+        <div className='header-outer'>
+            <div className='header'> 
+                <Link to='/'>shoppy logo</Link>
+                <nav>
+                    <Link to ='/all'>Products</Link>
+                    <Link to ='/cart'>MyCart</Link>
+                    <Link to ='/login'>Login</Link>
+                    <Link to = '/signup'>Signup</Link>
+                </nav>
+            </div>
         </div>
     );
 }
 
+// <a> 대신 <Link> 태그 사용하기 
+// <Link to = '라우터에서 적은 경로'>shoppy logo</Link> 누르면 index 페이지 나오게 해댜댕
