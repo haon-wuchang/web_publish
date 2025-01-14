6aeba6161004902ab07b00bf39463799730d72f6
haon <haoony@naver.com>
2025-01-14 13:37:38 +0900

    ㅇ

--- shoppy/client/src/components/Footer.jsx
+++ shoppy/client/src/components/Footer.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div>
-            footer
+        <div className='footer-outer'>
+            <div className='footer'>
+                <p>㈜더블유
+                대표자 : 이뀨유
+                주소 : 서울특별시 강남구 테헤로 00, 120층(역삼동, 센터필드)
+                사업자등록번호 : 111-11-11111 </p>
+                <p>통신판매업신고 : 제2호-서울강남-0호 사업자정보확인
+                개인정보보호책임자 : 피망
+                호스팅서비스 : ㈜더블유
+                고객센터 : 1111-5117</p>
+                이메일 : cs_help@pt.co.kr
+                운영시간 : 평일 09:00~18:00 (점심시간 : 12:30~13:30)
+                COPYRIGHT ⓒ ㈜더블유아 ALL RIGHTS RESERVED.
+            </div>
         </div>
     );
 }
