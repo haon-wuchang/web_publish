f13f0ac0c4f51010160250255987a45d19ffc825
haon <haoony@naver.com>
2025-01-15 16:58:50 +0900

    ㅇ

--- haon-practice/practice1/react-client/src/App.js
+++ haon-practice/practice1/react-client/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './pages/Layout.jsx';
+import Home from './pages/Home.jsx';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<Layout />}/>
+          <Route index element = {<Home />}/>
+          <Route path='/city' element = {<City />}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
-export default App;
+
+// 얘는 에어비앤비 만들어방 연숩 쥰내해라 자꾸 까먹으니깨 
