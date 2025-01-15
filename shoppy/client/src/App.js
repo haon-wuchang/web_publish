e80cda43999cd612a72df646ff5ce655d0e89974
haon <haoony@naver.com>
2025-01-15 15:02:06 +0900

    ;

--- shoppy/client/src/App.js
+++ shoppy/client/src/App.js
@@ -7,22 +7,38 @@ import Carts from './pages/Carts.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
 import DetailProduct from './pages/DetailProduct.jsx';
+import { useState } from 'react';
 
 export default function App() {
+  const addMomCart = (cartItem) => {
+    setCartList([...cartList,cartItem]);
+    setCartCount(cartCount + 1);
+  }  
+  // 이렇게 들어오는 아이템객체들을 하나에 모아ㅑㅇ대니까 배열에 객체들 차곡차곡담기
+  
+  //장바구니 리스트 : 배열로 만들어서 아이템객체들 모아두기, 바로바로업데이트돼야하니까 useState쓴거임
+  const [cartList,setCartList] = useState([]);
+  
+  // 장바구니 상품 갯수 저장 :  useState 생성
+  const [cartCount,setCartCount] = useState(0);
+  // cartCount 를 Layout 에 잇는 Header 의 carts 에 넘겨주면댐
+
   return (
     <div >
       <BrowserRouter>
         <Routes>
-          <Route path='/'element={<Layout />} >          
+          <Route path='/'element={<Layout cartCount ={cartCount} />} >          
             <Route index element={<Home />} /> 
             <Route path='/all' element={<Products />} /> 
             <Route path='/cart' element={<Carts />} /> 
             <Route path='/login' element={<Login />} /> 
             <Route path='/signup' element={<Signup />} /> 
-            <Route path='/products/:pid' element={<DetailProduct/>} /> 
+            <Route path='/products/:pid' element={<DetailProduct addCart={addMomCart}/>} /> 
           </Route >
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
+// 자식껄 부모한테 줄람녀 부모의 함수 이용
--- shoppy/client/src/pages/DetailProduct.jsx
+++ shoppy/client/src/pages/DetailProduct.jsx
@@ -2,8 +2,10 @@ import React, { useEffect,useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-export default function DetailProduct() {
+export default function DetailProduct({addCart}) {
     const {pid} = useParams(); 
+
+    const [size,setSize] = useState('XS');  // 장바구니 옵션 값 관리
 
     // 각ㄱ각의 상품들 정보를 가져오기 
     const [product,SetProduct] = useState({}); // product 가 json 데이터의 {} 이거 하나임 그래서 useState 초기값을 {} 로 한거임
@@ -16,9 +18,22 @@ export default function DetailProduct() {
                 })                  
             })       
             .catch(error => console.log(error));
-    },[]);
-    
+    },[]);    
 
+    //장바구니 추가 버튼 이벤트
+    const addCartItem = ()=> {
+        // console.log(pid, product.price,size,1);
+        // 장바구니 추가 항목 :{ pid, price, size, qty}
+        const cartItem = {
+            "pid" : product.pid,
+            "size" : size,
+            "price": product.price,
+            "qty" : 1
+        }
+        addCart(cartItem); // 부모인 App.js 의 addMomCart함수 호출됨 = addCart 에 장바구니추가항목만들어서 넘기면 됨
+    } 
+
+    
     return (
         <div className='content'>
             <div className='product-detail'>
@@ -31,7 +46,8 @@ export default function DetailProduct() {
                     <li className="product-detail-subtitle">{product.info}</li>
                     <li>
                         <span className='product-detail-select1'>옵션 : </span>
-                        <select className='product-detail-select2'>
+                        <select className='product-detail-select2'
+                            onChange={(e)=> setSize(e.target.value)}>
                             <option value="XS">XS</option>
                             <option value="S">S</option>
                             <option value="M">M</option>
@@ -40,7 +56,9 @@ export default function DetailProduct() {
                         </select>
                     </li>
                     <li>
-                        <button type="button" className='product-detail-button'>장바구니 추가</button>
+                        <button type="button" 
+                            className='product-detail-button'
+                            onClick={addCartItem}>장바구니 추가</button>
                     </li>
                 </ul>
             </div>
