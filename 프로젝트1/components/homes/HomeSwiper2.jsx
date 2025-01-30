aa41bea21dcb020c323f9f28ba31c7363a65e7a4
haon <haoony@naver.com>
2025-01-30 13:54:33 +0900

    ㅎ

--- 프로젝트1/components/homes/HomeSwiper2.jsx
+++ 프로젝트1/components/homes/HomeSwiper2.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function HomeSwiper2() {
    
-        스와이퍼2 이미지 리스트는 해당 브랜드 이미지 3개씩 가져오는거라 상품리스트에서 3개씩만 가져오면 될듯
-        jsx 로 틀 구성만 하면됨
+    왼쪽 큰 이미지 = 기획전에서 가져옴
+    오른쪽 2개 상품 = 해당 브랜드 상품이미지 2개 가져옴
    
 
     return (
@@ -13,4 +13,3 @@ export default function HomeSwiper2() {
     );
 }
 
-// 스와이퍼2   / 마지막 스와이퍼페이지에서 넘어가지않음 4페이지에서 1페이지로 x
