6bddc12a198a884150ce02c8696aee41c33fb614
haon <haoony@naver.com>
2025-02-07 10:54:07 +0900

    5

--- shoppy/client/src/components/ImageUpload.jsx
+++ shoppy/client/src/components/ImageUpload.jsx
@@ -1,39 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react'; // 2-2.
 import Form from 'react-bootstrap/Form'; 
 import axios from 'axios'; 
 
 export default function ImageUpload({getFileName}) { 
+    //2-2. then에서 res.data로 받아온 oldFile 관리할 변수 선언
+    const [oldFile,setOldFile] = useState('');
+        // 3. 올드파일은 새로운파일이 선택됏을떄 같이 값을 보내야한다
+
     const formData = new FormData(); 
 
     const handleFileUpload = (e) => {
-        console.log(e.target.files[0]); 
-        
-        // formData.append('file',넘어오는객체);
+        console.log(e.target.files[0]);   // 새로운파일
         formData.append('file',e.target.files[0]); 
-        for(const [key,value] of formData.entries()){
-            console.log(`key -- ${JSON.stringify(key)}`);   
-            console.log('value',value);                     
-        }
+        // for(const [key,value] of formData.entries()){
+        //     console.log(`key -- ${JSON.stringify(key)}`);   
+        //     console.log('value',value);                     
+        // } 얘는 필요없나바?
+        //3-1.
+        formData.append('oldFile',oldFile); // 이전파일명
         
         axios.post('http://localhost:9000/uploads',formData)
-            .then(res => {
-                console.log('res==',res.data);
-                getFileName(res.data); 
-            })  
-            .catch(error => console.log(error));
+        .then(res => {
+            console.log('res==',res.data);// 2-0.oldFile 잘 넘어오는지 확인
+            getFileName(res.data); //2-1. 올드파일은 뉴프로덕트컴포넌트까지 갈 필요는없음
+            setOldFile(res.data.oldFile); //2-3.
+        })  
+        .catch(error => console.log(error));
     }
+    
+    console.log('oldFile==',oldFile); //2-4.잘가져오는지 확인
+    
 
     return (
         <div>
             <Form.Control 
                 type='file' onChange={(e)=>{handleFileUpload(e)}}>
-            </Form.Control>
-                       
+            </Form.Control>                       
         </div>
     );
 }
 
 
 
-// <Form.Control accept='image/*'  //타입이 이미지인 애들만 가져오게 제한하는거임
+// <Form.Control accept='image/*'  //accept 는 (타입이 이미지인 애들만 가져오게) 제한하는거임
 
--- shoppy/server/controller/uploadController.js
+++ shoppy/server/controller/uploadController.js
@@ -6,9 +6,7 @@ const storage = multer.diskStorage({
       cb(null, 'upload_files/')
     },
     filename: function (req, file, cb) {
-        // 파일명 중복방지를 위한 새로운 파일명 생성
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random()*1e9);
-      // cd(null, 커스텀한내용을가진 변수 + file.originalname);
       cb(null, uniqueSuffix + '-' + file.originalname); 
     }
   })
@@ -20,12 +18,24 @@ export const fileUpload = (req,res) => {
         if(err) {
             console.log(err);            
         } else {
-            res.json({ //
-                // 폴더에 저장된 이미지파일명
-                "uploadFileName" : res.req.file.path,  // res => multer가 보내온 결과를 받는거임
-                // 사용자가 선택한 기존의 이미지파일명
-                "sourceFileName" : req.file.originalname, // req => fileUpload의 req 임
-            });
+          console.log(req.file); // 1-1.업로드완료후 넘어오는 객체 확인 
+          //1-2.업로드 완료된 파일 정보 서버에서 확인하기
+          // { 서버 결과값
+          //   fieldname: 'file',
+          //   originalname: '1.webp',
+          //   encoding: '7bit',
+          //   mimetype: 'image/webp',
+          //   destination: 'upload_files/',
+          //   filename: '1738892169947-867116037-1.webp',
+          //   path: 'upload_files\\1738892169947-867116037-1.webp',
+          //   size: 45842
+          // } 1-3.여기 filename앞에 파일저장되는폴더명이 없음  그래서 json보낼때는 filename 으로 보내면된다
+
+            res.json({ 
+                "uploadFileName" : res.req.file.path,  
+                "sourceFileName" : req.file.originalname, 
+                "oldFile": res.req.file.filename // =>  1-4.'1738892169947-867116037-1.webp' 이거말하는거임
+            }); // 1-5.우리는 이값을 이미지업로드컴포넌트로 보낼꺼라 앞에 res 붙인거임
         }
     });
 }
