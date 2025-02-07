5c9bbfc8ff960236a42cda5391c2ae0439841efb
haon <haoony@naver.com>
2025-02-07 11:38:44 +0900

    5

--- shoppy/client/src/components/ImageUpload.jsx
+++ shoppy/client/src/components/ImageUpload.jsx
@@ -10,25 +10,24 @@ export default function ImageUpload({getFileName}) {
     const formData = new FormData(); 
 
     const handleFileUpload = (e) => {
-        console.log(e.target.files[0]);   // 새로운파일
+        // console.log(e.target.files[0]);   // 새로운파일
         formData.append('file',e.target.files[0]); 
-        // for(const [key,value] of formData.entries()){
-        //     console.log(`key -- ${JSON.stringify(key)}`);   
-        //     console.log('value',value);                     
-        // } 얘는 필요없나바?
         //3-1.
-        formData.append('oldFile',oldFile); // 이전파일명
+        formData.append('oldFile',oldFile); // 이전파일명 보내는내용 추가
         
-        axios.post('http://localhost:9000/uploads',formData)
-        .then(res => {
-            console.log('res==',res.data);// 2-0.oldFile 잘 넘어오는지 확인
-            getFileName(res.data); //2-1. 올드파일은 뉴프로덕트컴포넌트까지 갈 필요는없음
-            setOldFile(res.data.oldFile); //2-3.
-        })  
-        .catch(error => console.log(error));
+        axios.post('http://localhost:9000/uploads',formData,{
+            //4.formData에 문자(oldFile)도 포함해서 보낼때는 옵션을 추가해야한다
+            headers :{ 'Content-Type':'multipart/form-data'}, // => 파일과 문자 등 데이터가 추가됐을때 사용하는 문법( 파일만 넘어갈때는 생략가능)
+            })
+            .then(res => {
+                // console.log('res==',res.data);// 2-0.oldFile 잘 넘어오는지 확인
+                getFileName(res.data); //2-1. 올드파일은 뉴프로덕트컴포넌트까지 갈 필요는없음
+                setOldFile(res.data.oldFile); //2-3.
+            })  
+            .catch(error => console.log(error));
     }
     
-    console.log('oldFile==',oldFile); //2-4.잘가져오는지 확인
+    // console.log('oldFile==',oldFile); //2-4.잘가져오는지 확인
     
 
     return (
--- shoppy/client/src/pages/NewProduct.jsx
+++ shoppy/client/src/pages/NewProduct.jsx
@@ -4,14 +4,17 @@ import ImageUpload from '../components/ImageUpload.jsx';
 
 export default function NewProduct() {
     const [fnames,setFnames] = useState({}); 
-
+// ㄷ.파일이 새로 바뀌면 값을 관리해야하니까 변수 선언
+    const [preview, setPreview] = useState('');
 
     const getFileName = (filesNames) => { 
-        console.log('fileName==>',filesNames);   
-        setFnames(filesNames);    
+        // console.log('fileName==>',filesNames);   
+        setFnames(filesNames);  
+        //ㄹ. 새로운 파일이 들어오면 해당 주소로 이동되게
+        setPreview(`http://localhost:9000/${filesNames.uploadFileName}`);
     }
-
-
+// ㄱ. 파일 등록전 미리보기 작업 => 업로드파일 경로
+ 
     return (
         <div className='content'>
             <h1>상품등록페이지</h1>
@@ -32,13 +35,16 @@ export default function NewProduct() {
                     <li>
                         <label>파일업로드</label>
                         <ImageUpload getFileName={getFileName}/> 
+                        {/*ㄴ.파일미리보기 태그 생성*/ }
+                        {/*  ㅁ. 프리뷰가 잇을때만 해당 칸 나오게  */}
+                        {preview && <img src={preview} alt="파일미리보기" style={{'width':'200px'}} />}
                     </li>
                     <li>
                         <input type="text" name='uploadFile' value={fnames.uploadFileName} /> 
                         <input type="text" name='sourceFile' value={fnames.sourceFileName}/>
                     </li>
                     <li>
-                        <button type='submit'>상품등록</button>
+                        <button type='submit'>파일등록</button>
                         <button type='reset'>취소</button>
                     </li>
                 </ul>
