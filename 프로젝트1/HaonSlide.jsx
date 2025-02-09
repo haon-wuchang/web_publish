80bd8bc64cf3e468bdf9c4bba892c76a10f57989
haon <haoony@naver.com>
2025-02-09 15:31:09 +0900

    d

--- 프로젝트1/HaonSlide.jsx
+++ 프로젝트1/HaonSlide.jsx
@@ -8,16 +8,8 @@ import { BsPause } from "react-icons/bs";
 export default function HaonSlide() {
     const [data,setData] = useState([]);  // 이미지 데이터 
     const [isPlay , setIsPlay] = useState(true); // 슬라이드 재생,멈춤 관리
-
-    const [page, setPage] = useState(1); // 페이지 관리(시작 페이지)
-    const itemsPerPage = 1; // Num = 한 페이지에 보여줄 데이터 갯수
-    const totalItem = data.length; // 데이터 총 갯수    
-    const totalPages = Math.ceil(totalItem / itemsPerPage); // 총 페이지 갯수 계산
-    const currentPage = 1;
-    const [sliceData, setSliceData] = useState([]); // 현재 페이지에서 보여줄 리스트(slice)
-    // 리스트 slice 범위
-    const endIdx = (page * itemsPerPage); // 반드시 endIdx가 startIdx보다 먼저 선언되어야 함
-    const startIdx = endIdx - itemsPerPage;
+    const [current, setCurrent] = useState(0);  // 이미지 index 값 
+    const length = data.length;  // 이미지 전체 갯수    
 
     useEffect(() => {
         axios.get('/data/haon.json')
@@ -25,52 +17,50 @@ export default function HaonSlide() {
             .catch((error) => console.log(error));
     }, []);
 
-    useEffect(() => {
-        setSliceData(data.slice(startIdx, endIdx));
-    }, [page, data]);
-
-
-    const handlePrev = () => {
-        if (page > 1) {
-            setPage(page-1);
-        } else if(page === 1){
-            setPage(totalPages)
-        }
-    }
-
-    const handleNext = () => {
-        if(page < totalPages){
-            setPage(page+1)
-        } else if(page === totalPages){
-            setPage(1);
-        }
-    }
-    const handlePlay = () => {
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+      };
+    
+      const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+      };
+      const handlePlay = () => {
         setIsPlay(true);
     }
     const handleStop = () => {
         setIsPlay(false);
     }
-
+    
+    useEffect(() => {
+    const interval = setInterval(() => {
+        if(isPlay === true){
+            nextSlide();
+        }else{
+            handleStop();
+        }
+    }, 10000); 
+    
+    return () => clearInterval(interval); 
+    }, [current, length,isPlay]);
 
     return (
         <>
         <div className='haon-slide-box'>
-            <FaArrowAltCircleLeft className='slide-leftBtn' onClick={handlePrev}/>
-            {sliceData.map((item)=>
+            <FaArrowAltCircleLeft className='slide-leftBtn' onClick={prevSlide}/>
+            {data.map((item,index)=>
             <>
-                <div>
-                    <img src={item.item1}/>
+                <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                    {index === current && <img src={item.item1}/>}
                 </div>             
-                <div>
-                    <img src={item.item3}/>
+                <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                    {index === current && <img src={item.item2}/>}
                 </div>             
-                <div>
-                    <img src={item.item2}/>
+                <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                    {index === current && <img src={item.item3}/>}
                 </div>     
             </>        
             )}
-            <FaArrowAltCircleRight className='slide-rightBtn' onClick={handleNext} />    
+            <FaArrowAltCircleRight className='slide-rightBtn' onClick={nextSlide} />    
             {isPlay === true ? 
                 (<CiPlay1 className='slide-playBtn' onClick={handleStop}/>):
                 (<BsPause className='slide-stopBtn' onClick={handlePlay}/> )                                 
