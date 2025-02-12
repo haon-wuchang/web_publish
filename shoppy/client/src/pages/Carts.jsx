9473f62fa9ba13f7d54a3639600d25e7844514e5
haon <haoony@naver.com>
2025-02-12 15:08:27 +0900

    5

--- shoppy/client/src/pages/Carts.jsx
+++ shoppy/client/src/pages/Carts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 
 export default function Carts() {
@@ -12,30 +12,37 @@ export default function Carts() {
      // map 실행한결과는 새로운배열에 만들어짐 ( 그래서 따로 변수로 배열안만들어도 됨 ,push 도 안해도되고)
     // console.log('pids',pids);
 
-    //5. axios 이용하여 db 연동작업
-    axios.post('http://localhost:9000/product/cartItems',{'pids':pids}) //5-1. 오브젝트리터럴형태로 넘어가야해서 {pids} 
-        .then(
-            res =>{ 
-            console.log('res=',res.data);   //6. 레파지토리-컨트롤러에서 보낸값 받음         
-           //6-2. cartItems 에 res.data 의 정보 추가
+    //7-1.
+    useEffect(()=>{
+        if(pids.length > 0){
+            //5. axios 이용하여 db 연동작업
+            axios
+            .post('http://localhost:9000/product/cartItems',{'pids':pids}) //5-1. 오브젝트리터럴형태로 넘어가야해서 {pids} 
+            .then(
+                res =>{ 
+                console.log('res=',res.data);   //6. 레파지토리-컨트롤러에서 보낸값 받음         
+            //6-2. cartItems 에 res.data 의 정보 추가
             const updateCartItems = cartItems.map((item,index)=>
-                                        item.pid === res.data[index].pid ?
-                                        {...item, 
-                                            'pname':res.data[index].pname, 
-                                            'price':res.data[index].price,
-                                            'image':res.data[index].image,
-                                            'description':res.data[index].description
-                                        } : item
-                                    );
-            //6-3.updateCartItems 를 setCartItems 에 넣어주기
-            setCartItems(updateCartItems);           
-        })
-        .catch(error => console.log(error));
+                                            item.pid === res.data[index].pid ?
+                                            {...item, 
+                                                'pname':res.data[index].pname, 
+                                                'price':res.data[index].price,
+                                                'image':res.data[index].image,
+                                                'description':res.data[index].description
+                                            } : item
+                                        );
+                                        //6-3.updateCartItems 를 setCartItems 에 넣어주기
+                setCartItems(updateCartItems);           
+                                    })
+            .catch(error => console.log(error));                                    
+           }
+        },[]);
+        //7. 근데 이케하면 콘솔이 무한으로 찍히게 된다 얘를 useEffect 를 사용해서 react 가 관리하게 만들어준다
         console.log('cartItems',cartItems);
         
     return (
         <div className='content'>
-            <table border='1'>
+            <table border='1' style={{width:'1400px'}}>
                 <tr>
                     <th>image</th>
                     <th>pid</th>
@@ -45,15 +52,18 @@ export default function Carts() {
                     <th>qty</th>
                     <th>price</th>
                 </tr> 
-                {/* <tr>
-                    <td>image</td>
-                    <td>{item.pid}</td>
-                    <td>{item.pname}</td>
-                    <td>{item.qty}</td>
-                    <td>{item.qty}</td>
-                    <td>{item.qty}</td>
-                    <td>{item.qty}</td>
-                </tr>  */}
+                {/* 7-2. */}
+                {cartItems&& cartItems.map((cartItems)=>
+                <tr style={{textAlign:'center', fontSize:'25px'}}>
+                    <td><img src={cartItems.image} alt="" style={{width:'200px'}} /></td>
+                    <td>{cartItems.pid}</td>
+                    <td>{cartItems.pname}</td>
+                    <td>{cartItems.description}</td>
+                    <td>{cartItems.size}</td>
+                    <td>{cartItems.qty}</td>
+                    <td>{cartItems.price}</td>
+                </tr> 
+                )}
             </table>
         </div>
     );
